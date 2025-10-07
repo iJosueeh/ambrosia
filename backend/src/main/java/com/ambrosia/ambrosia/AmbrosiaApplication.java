60fb281fce107d393d4147580968dc85a8c43582
IJosueeh01 <royer.tanta27@gmail.com>
2025-10-07 10:02:59 -0500

    feat: Back-End inicializado

---
+++ backend/src/main/java/com/ambrosia/ambrosia/AmbrosiaApplication.java
@@ -0,0 +1,13 @@
+package com.ambrosia.ambrosia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmbrosiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmbrosiaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ambrosia/ambrosia/AmbrosiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambrosia.ambrosia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmbrosiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
